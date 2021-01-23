e688ab4adc6f3be955fe283663d55948b2634cde
yutinggan <gan.yut@husky.neu.edu>
2021-01-23 17:38:23 -0500

    A1

--- app/src/main/java/edu/neu/madcourse/numad21s_yutinggan/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_yutinggan/MainActivity.java
@@ -3,6 +3,9 @@ package edu.neu.madcourse.numad21s_yutinggan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button aboutButton = findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView statusText = findViewById(R.id.statusText);
+                statusText.setText("Name: Yuting Gan \nEmail: gan.yut@northeastern.edu");
+            }
+        });
     }
+
+
 }
