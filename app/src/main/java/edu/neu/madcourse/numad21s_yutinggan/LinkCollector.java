56a4346f0cf46962982d0055d537a8dd0b8966c8
yutinggan <gan.yut@husky.neu.edu>
2021-02-10 21:01:11 -0500

    try example view example

--- app/src/main/java/edu/neu/madcourse/numad21s_yutinggan/LinkCollector.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_yutinggan/LinkCollector.java
@@ -31,7 +31,7 @@ public class LinkCollector extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_link_collector);
 
         init(savedInstanceState);
 
