7b9d6565817ab3196339ac9d897e37f35cf9a465
yutinggan <gan.yut@husky.neu.edu>
2021-02-17 23:07:48 -0500

    add permission code

--- app/src/main/java/edu/neu/madcourse/numad21s_yutinggan/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_yutinggan/MainActivity.java
@@ -1,11 +1,17 @@
 package edu.neu.madcourse.numad21s_yutinggan;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     public Button clickyButton;
     public Button locatorButton;
     public Button linkCollectorButton;
+    private final int locationPermissionRequestCode = 1;
+
+    protected void requestPermission(String permissionType,
+                                     int requestCode) {
+        ActivityCompat.requestPermissions(this, new String[]{permissionType}, requestCode );
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +58,52 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Intent locatorIntent = new Intent(MainActivity.this, Locator.class);
         locatorButton = findViewById(R.id.button_locator);
         locatorButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
-                Intent locatorIntent = new Intent(MainActivity.this, Locator.class);
-                startActivity(locatorIntent);
+//                Intent locatorIntent = new Intent(MainActivity.this, Locator.class);
+//                startActivity(locatorIntent);
+                this.getLocationPermission();
+            }
+
+
+            private void getLocationPermission() {
+                if (ContextCompat.checkSelfPermission(locatorButton.getContext(),
+                        android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                    startActivity(locatorIntent);
+
+                } else {
+                    requestPermission(android.Manifest.permission.ACCESS_FINE_LOCATION, locationPermissionRequestCode);
+
+                }
             }
         });
 
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case locationPermissionRequestCode:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Intent locatorIntent = new Intent(MainActivity.this, Locator.class);
+                    startActivity(locatorIntent);
+
+                } else {
+
+                    Toast.makeText(this,
+                            "Unable to show location - permission required", Toast.LENGTH_LONG).show();
+                }
+                return;
+        }
     }
 
 
