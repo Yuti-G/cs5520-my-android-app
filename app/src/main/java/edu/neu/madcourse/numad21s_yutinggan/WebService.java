3357d00c21b6ff3e0e979a136b7ec9519ce5ad50
yutinggan <gan.yuti@gmail.com>
2021-03-07 11:07:15 -0500

    finished ui

---
+++ app/src/main/java/edu/neu/madcourse/numad21s_yutinggan/WebService.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21s_yutinggan;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_service);
+    }
+}
